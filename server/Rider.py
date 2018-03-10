7b73e2d1cb8d73799441a9131ab16031fbc7efab
Rob Willoughby <robgwilloughby@gmail.com>
2018-03-10 13:25:29 -0800

    First implementation

---
+++ server/Rider.py
@@ -0,0 +1,8 @@
+class Rider:
+
+    def __init__(self, location, time):
+        self.location = location
+        self.start = time - 1
+        self.end = time
+        self.duration = 2
+        self.load = 1
