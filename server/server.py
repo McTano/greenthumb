a706a8e8afbbb0488a0b9276e37fba6424cf0ce0
Tristan <tsmackinlay@gmail.com>
2018-03-10 13:10:57 -0800

    added put user and get rides routes

--- server/server.py
+++ server/server.py
@@ -1,6 +1,21 @@
 from flask import Flask
+from flask import request
+from flask import json
+
+from server import RideManager
+
 app = Flask(__name__)
+
+rideManager = RideManager.getInstance()
 
 @app.route("/")
 def hello():
-	return "Hello World!"
+	return "Hello, Squirrels!"
+
+@app.route("/users/new", methods=['PUT'])
+def addUser():
+	rideManager.addUser(request.user)
+
+@app.route("/rides", methods=['GET'])
+def findRides():
+	return "get your own damn ride"
