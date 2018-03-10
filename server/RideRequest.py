0e0e14782287a6d210f96e34f0c4a15dbea82aa5
Unknown <ericjmyuan@gmail.com>
2018-03-10 12:27:36 -0800

    skeleton + some implementation.

---
+++ server/RideRequest.py
@@ -0,0 +1,6 @@
+class RideRequest:
+    def __init__(self):
+        RideRequest.start
+        RideRequest.destination
+        RideRequest.user
+        RideRequest.location
---
+++ server/User.py
@@ -0,0 +1,7 @@
+class User:
+    def __init__(self, start, dest, hasCar, seats, vehicle):
+        User.start = start
+        User.dest = dest
+        User.hasCar = hasCar
+        User.seats = seats
+        User.vehicle = vehicle
