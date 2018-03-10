1b1b05c1ee435dabe5c76193527bfb67456588b6
Unknown <ericjmyuan@gmail.com>
2018-03-10 12:49:49 -0800

    implemented User

--- server/RideRequest.py
+++ server/RideRequest.py
@@ -1,6 +1,9 @@
 class RideRequest:
-    def __init__(self):
-        RideRequest.start
-        RideRequest.destination
-        RideRequest.user
-        RideRequest.location
+    def __init__(self, start, destination, user, time):
+        self.start
+        self.destination
+        self.user
+        self.time
+
+    def getStart(self):
+        return
